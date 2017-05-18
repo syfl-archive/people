58d242e361219c0e981ebc38da2e7d55261b114e
Ricardo Silva <rick.silva@gmail.com>
2017-05-18 15:50:05 +0100

    Person Model :: must have a last name

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Person, type: :model do
     expect(person).not_to be_valid
     expect(person.errors[:first_name]).to include("can't be blank")
   end
+
+  it 'must have a last name' do
+    person = Person.new
+    expect(person).not_to be_valid
+    expect(person.errors[:last_name]).to include("can't be blank")
+  end
 end
