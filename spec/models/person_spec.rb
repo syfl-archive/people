738564b1b6e275adff7489b3772835b2e60bb81b
Ricardo Silva <rick.silva@gmail.com>
2017-05-18 16:52:48 +0100

    Person Model :: has an optional middle name

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Person, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.not_to validate_presence_of(:middle_name) }
   end
 
   describe '#full_name' do
