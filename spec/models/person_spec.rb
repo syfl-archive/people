758bb52d9eba27415ed4d9b1deaa590ce6bbf54f
Ricardo Silva <rick.silva@gmail.com>
2017-05-18 16:37:10 +0100

    Person Model :: Refactoring - Step 1

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,18 +1,19 @@
 RSpec.describe Person, type: :model do
+  subject(:person) { Person.new }
+
   it 'must have a first name' do
-    person = Person.new
     expect(person).not_to be_valid
     expect(person.errors[:first_name]).to include("can't be blank")
   end
 
   it 'must have a last name' do
-    person = Person.new
     expect(person).not_to be_valid
     expect(person.errors[:last_name]).to include("can't be blank")
   end
 
   it 'can construct a full name from first and last names' do
-    person = Person.new(first_name: 'John', last_name: 'Smith')
+    person.first_name = 'John'
+    person.last_name = 'Smith'
     expect(person.full_name).to eq 'John Smith'
   end
 end
